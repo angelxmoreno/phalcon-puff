79d12e9270816cf7e63ece2209f8ae97fedb15be
Angel S. Moreno <angelxmoreno@gmail.com>
2016-08-14 01:30:49 -0400

    Moved home view to statics

--- config/routes.php
+++ config/routes.php
@@ -2,5 +2,6 @@
 namespace AXM\Controllers;
 
 $router->removeExtraSlashes(true);
+$router->addStatic('/', 'home');
 $router->addStatic('/about', 'about');
 $router->addStatic('/contact', 'contact');
