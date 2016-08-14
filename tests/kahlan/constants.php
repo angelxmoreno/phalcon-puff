67a9cb11f6e8fdf97da31744b45a5b32e5e350e0
Angel S. Moreno <angelxmoreno@gmail.com>
2016-08-14 03:14:35 -0400

    Kahlan tests setup

---
+++ tests/kahlan/constants.php
@@ -0,0 +1,11 @@
+<?php
+define('KAHLAN_PATH', __DIR__ . DS);
+define('SPEC_PATH', KAHLAN_PATH . 'spec' . DS);
+define('TESTS_PATH', dirname(KAHLAN_PATH) . DS);
+define('ROOT_PATH', dirname(dirname(KAHLAN_PATH)) . DS);
+define('APP_PATH', ROOT_PATH . 'src' . DS);
+define('CONFIG_PATH', ROOT_PATH . 'config' . DS);
+define('CACHE_PATH', ROOT_PATH . 'cache' . DS);
+define('VIEWS_PATH', ROOT_PATH . 'views' . DS);
+define('VENDOR_PATH', ROOT_PATH . 'vendor' . DS);
+define('LOGS_PATH', ROOT_PATH . 'logs' . DS);
