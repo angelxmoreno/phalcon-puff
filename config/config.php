45da07b04e3aaa06a5c301579495686197d3818b
Angel S. Moreno <angelxmoreno@gmail.com>
2016-08-20 02:31:03 -0400

    Feature/app env (#5)
    * Integrating .env file with config.php
    
    * Added env powered services

---
+++ config/config.php
@@ -0,0 +1,81 @@
+<?php
+return [
+    'database' => [
+        'adapter' => ucfirst(parse_url(getenv('DATABASE_URL'), PHP_URL_SCHEME)),
+        'host' => parse_url(getenv('DATABASE_URL'), PHP_URL_HOST),
+        'port' => parse_url(getenv('DATABASE_URL'), PHP_URL_PORT),
+        'username' => parse_url(getenv('DATABASE_URL'), PHP_URL_USER),
+        'password' => parse_url(getenv('DATABASE_URL'), PHP_URL_PASS),
+        'dbname' => parse_url(getenv('DATABASE_URL'), PHP_URL_PATH),
+        'charset' => 'utf8',
+    ],
+    'sessions' => [
+        'adapter' => ucfirst(parse_url(getenv('SESSION_URL'), PHP_URL_SCHEME)),
+        'host' => parse_url(getenv('SESSION_URL'), PHP_URL_HOST),
+        'port' => parse_url(getenv('SESSION_URL'), PHP_URL_PORT),
+        'auth' => parse_url(getenv('SESSION_URL'), PHP_URL_PASS),
+        'persistent' => true,
+        'lifetime' => 2592000, //30 days
+        'prefix' => 'session_'
+    ],
+    'application' => [
+        'controllersDir' => APP_PATH . 'controllers' . DS,
+        'modelsDir' => APP_PATH . 'models' . DS,
+        'migrationsDir' => ROOT_PATH . 'migrations' . DS,
+        'viewsDir' => VIEWS_PATH,
+        'pluginsDir' => APP_PATH . 'plugins' . DS,
+        'libraryDir' => APP_PATH . 'library' . DS,
+        'cacheDir' => CACHE_PATH,
+        'baseUri' => ''
+    ],
+    'view' => [
+        'options' => [
+            'compiledPath' => CACHE_PATH . 'compiled' . DS,
+            'compiledExtension' => '.php',
+            'compiledSeparator' => '__',
+            'stat' => true,
+            'prefix' => 'view_',
+            'compileAlways' => true,
+        ]
+    ],
+    'logger' => [
+        'adapters' => [
+            'FirePHP' => '',
+            'File' => LOGS_PATH . 'app.log',
+        ]
+    ],
+    'cache' => [
+        'default' => [
+            'frontend' => [
+                'Data' => [
+                    'lifetime' => 172800
+                ]
+            ],
+            'backend' => [
+                'Redis' => [
+                    'adapter' => ucfirst(parse_url(getenv('CACHE_URL'), PHP_URL_SCHEME)),
+                    'host' => parse_url(getenv('CACHE_URL'), PHP_URL_HOST),
+                    'port' => parse_url(getenv('CACHE_URL'), PHP_URL_PORT),
+                    'auth' => parse_url(getenv('CACHE_URL'), PHP_URL_PASS),
+                    'persistent' => true
+                ]
+            ]
+        ],
+        'database' => [
+            'frontend' => [
+                'Data' => [
+                    'lifetime' => 600 //10 minutes
+                ]
+            ],
+            'backend' => [
+                'Redis' => [
+                    'adapter' => ucfirst(parse_url(getenv('MODELS_CACHE_URL'), PHP_URL_SCHEME)),
+                    'host' => parse_url(getenv('MODELS_CACHE_URL'), PHP_URL_HOST),
+                    'port' => parse_url(getenv('MODELS_CACHE_URL'), PHP_URL_PORT),
+                    'auth' => parse_url(getenv('MODELS_CACHE_URL'), PHP_URL_PASS),
+                    'persistent' => true
+                ]
+            ]
+        ]
+    ]
+];
